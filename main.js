ae25c40aa7c9e07d3303405914d93cfa4f019626
Nalin Joshi <nalinjoshi629@gmail.com>
2023-01-10 12:46:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+function makeActive() {
+    var element = document.getElementById(".active");
+    element.classList.add("active");
+ }
