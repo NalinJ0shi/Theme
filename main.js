34c0e816991dec287bb2d0394273994ab7cffe79
Nalin Joshi <120622015+NalinJ0shi@users.noreply.github.com>
2023-01-16 21:17:54 +0530

    color commits

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-let toggle = document.querySelector('.toggle');
-let menu = document.querySelector('.menu');
-function makeActive() {
-    var element = document.getElementById(".active");
-    element.classList.add("active");
- }
+var selector ='.menu';   
+      
+$('.menu').on('click', function() {
+    var $self = $(this);
+    $self.hasClass('active') ? $self.removeClass('active').addClass('remove') : $self.removeClass('remove').addClass('active');
+});
+
+function red(){
+    document.body.style.backgroundColor = "#ff2972";
+}
+function yellow(){
+    document.body.style.backgroundColor = "#e2ff29";
+}
+function purple(){
+    document.body.style.backgroundColor = "#6929ff";
+}
+function blue(){
+    document.body.style.backgroundColor = "#29bbff";
+}
+function green(){
+    document.body.style.backgroundColor = "#29ff89";
+}
+function pink(){
+    document.body.style.backgroundColor = "#d129ff";
+}
+function voilet(){
+    document.body.style.backgroundColor = "#7029ff";
+}
+
+
